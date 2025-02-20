6bb84b9f0bd94586ab1446c7f31594311c9e90e8
Deklen Nates <dmn230001@utdallas.edu>
2025-02-20 17:14:03 -0600

    Messing around with ptrs

--- Random Test/randomTest.cpp
+++ Random Test/randomTest.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <memory>
+#include <cctype>
 
 using namespace std;
+
+void *getNumbers(int);
 
 int main(){
 
@@ -8,12 +12,17 @@ int main(){
 
     const int SIZE = 3;
     int *aptr = new int[SIZE];
+    aptr[0] = 5;
 
-
-
+    cout << aptr[0];
     delete ptr;
     delete [] aptr;
     aptr = nullptr;
 
+    return 0;
+}
+
+void *getNumbers(int val){
+    int *ptr = new int[val];
     return 0;
 }
