1eaeef3caf89c7bf14dfa7e19d4fe5bb2867c329
Deklen Nates <dmn230001@utdallas.edu>
2025-04-08 16:29:25 -0500

    Update lazySearch.cpp

--- Lazy Search/lazySearch.cpp
+++ Lazy Search/lazySearch.cpp
@@ -2,25 +2,35 @@
 
 using namespace std;
 
-const int EFFORT = 3
+// Accuracy
+const int EFFORT = 3;
 
 int lazySearch(int *arr, int arrSize, int target);
 
 int main(){
+
     int size, target;
+
     cout << "Input the size of the array: ";
     cin >> size;
+
     int arr[size];
+    
     cout << "Input the elements:\n";
     for(int i = 0; i < size; i++){
         cout << "Element #" << i + 1 << ": ";
         cin >> arr[i];
     }
+    
     cout << "Input the target: ";
     cin >> target;
+    
     cout << lazySearch(arr, size, target);
+    
+    return 0;
 }
 
+// Always works
 int lazySearch(int *arr, int arrSize, int target){
     int index = -1;
     for(int i = 0; i < EFFORT; i++){
